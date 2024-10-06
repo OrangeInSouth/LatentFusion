b5f1cc32783607c7b68162a5b7d3121e1dfdd506
ychuang <ychuang@ir.hit.edu.cn>
2024-10-06 17:15:26 +0800

    a

--- utils/anchors/obtain_layer_level_anchors.py
+++ utils/anchors/obtain_layer_level_anchors.py
@@ -1,12 +1,12 @@
 import torch
 
 anchors_dir = "/share/home/fengxiaocheng/ychuang/LatentFusion/experiments/anchor_embeddings"
-anchor_prex = "llama2-13b_mistral-7b_200000anchors_seed1"
-anchor_path = f"{anchor_path}/{anchor_prex}.pt"
+anchor_prefix = "llama2-13b_mistral-7b_200000anchors_seed1"
+anchor_path = f"{anchors_dir}/{anchor_prefix}.pt"
 models = ["llama2-13b", "mistral-7b"]
 layer_pair = [40, 32]
 
-output_path = f"{anchor_path}/{anchor_prex}_layer{'-'.join([str(i) for i in layer_pair])}.pt"
+output_path = f"{anchor_path}/{anchor_prefix}_layer{'-'.join([str(i) for i in layer_pair])}.pt"
 
 
 state = torch.load(anchor_path)
