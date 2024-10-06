52dda7c63198091738d1b5d344dc53eaddc85759
ychuang <ychuang@ir.hit.edu.cn>
2024-10-06 20:07:28 +0800

    update models

--- models/update.sh
+++ models/update.sh
@@ -1,5 +1,5 @@
-src_path=/data/home/cpfu/ychuang/DeepEN_v0601_ychuang/models/
-tgt_path=/data/home/cpfu/anaconda3/envs/ychuang/lib/python3.10/site-packages/transformers/models/
+src_path=/share/home/fengxiaocheng/ychuang/LatentFusion/models/
+tgt_path=/share/home/fengxiaocheng/miniconda3/envs/ychuang/lib/python3.10/site-packages/transformers/models/
 
 cp ${src_path}/modeling_llama.py ${tgt_path}/llama/modeling_llama.py
 cp ${src_path}/modeling_mistral.py ${tgt_path}/mistral/modeling_mistral.py
