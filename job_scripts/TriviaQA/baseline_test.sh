541eb89063842a9158d3b5bfbcbeace64455296a
ychuang <ychuang@ir.hit.edu.cn>
2024-10-07 13:43:21 +0800

    baseline

--- job_scripts/TriviaQA/baseline_test.sh
+++ job_scripts/TriviaQA/baseline_test.sh
@@ -20,15 +20,6 @@ proj_path=/share/home/fengxiaocheng/ychuang/LatentFusion
 export PYTHONPATH=${proj_path}
 cd ${proj_path}
 
-python src/main.py --config confs/TriviaQA/llama2-13b_mistral-7b.json \
---models llama2-13b mistral-7b \
---layer-alignment 40 32 \
---anchors-path ${proj_path}/experiments/anchor_embeddings/llama2-13b_mistral-7b_200000anchors_seed1_1.pt \
---embedding-projection-path ${proj_path}/experiments//embedding_projection/EstimationEmbeddingProjection_200000anchors_seed1.pt \
---result_save_dir ${proj_path}/experiments/TriviaQA/${mode}/llama2-13b_mistral-7b_${anchor_num}anchors_seed${seed} \
---sampling-anchor-num 20000 \
---ensemble_weight 0.8 0.2 \
---run_mode ${mode} 
 
 python src/main.py --config confs/TriviaQA/llama2-13b_mistral-7b.json \
 --models llama2-13b mistral-7b \
@@ -37,5 +28,36 @@ python src/main.py --config confs/TriviaQA/llama2-13b_mistral-7b.json \
 --embedding-projection-path ${proj_path}/experiments//embedding_projection/EstimationEmbeddingProjection_200000anchors_seed1.pt \
 --result_save_dir ${proj_path}/experiments/TriviaQA/${mode}/llama2-13b_mistral-7b_${anchor_num}anchors_seed${seed} \
 --sampling-anchor-num 20000 \
---ensemble_weight 1.0 0.0 \
+--ensemble_weight 0.001 0.999 \
 --run_mode ${mode} 
+
+
+python src/main.py --config confs/TriviaQA/llama2-13b_mistral-7b.json \
+--models llama2-13b mistral-7b \
+--layer-alignment 40 32 \
+--anchors-path ${proj_path}/experiments/anchor_embeddings/llama2-13b_mistral-7b_200000anchors_seed1_1.pt \
+--embedding-projection-path ${proj_path}/experiments//embedding_projection/EstimationEmbeddingProjection_200000anchors_seed1.pt \
+--result_save_dir ${proj_path}/experiments/TriviaQA/${mode}/llama2-13b_mistral-7b_${anchor_num}anchors_seed${seed} \
+--sampling-anchor-num 20000 \
+--ensemble_weight 0.999 0.001  \
+--run_mode ${mode} 
+
+# python src/main.py --config confs/TriviaQA/llama2-13b_mistral-7b.json \
+# --models llama2-13b mistral-7b \
+# --layer-alignment 40 32 \
+# --anchors-path ${proj_path}/experiments/anchor_embeddings/llama2-13b_mistral-7b_200000anchors_seed1_1.pt \
+# --embedding-projection-path ${proj_path}/experiments//embedding_projection/EstimationEmbeddingProjection_200000anchors_seed1.pt \
+# --result_save_dir ${proj_path}/experiments/TriviaQA/${mode}/llama2-13b_mistral-7b_${anchor_num}anchors_seed${seed} \
+# --sampling-anchor-num 20000 \
+# --ensemble_weight 0.8 0.2 \
+# --run_mode ${mode} 
+
+# python src/main.py --config confs/TriviaQA/llama2-13b_mistral-7b.json \
+# --models llama2-13b mistral-7b \
+# --layer-alignment 40 32 \
+# --anchors-path ${proj_path}/experiments/anchor_embeddings/llama2-13b_mistral-7b_200000anchors_seed1_1.pt \
+# --embedding-projection-path ${proj_path}/experiments//embedding_projection/EstimationEmbeddingProjection_200000anchors_seed1.pt \
+# --result_save_dir ${proj_path}/experiments/TriviaQA/${mode}/llama2-13b_mistral-7b_${anchor_num}anchors_seed${seed} \
+# --sampling-anchor-num 20000 \
+# --ensemble_weight 1.0 0.0 \
+# --run_mode ${mode} 
