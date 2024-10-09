0139c760b5a2a9abc8e2dbd0b22367cbcfc21634
ychuang <ychuang@ir.hit.edu.cn>
2024-10-09 15:45:08 +0800

    1million anchors

--- utils/anchors/merge_anchors.py
+++ utils/anchors/merge_anchors.py
@@ -3,7 +3,7 @@ import os
 from tqdm import tqdm
 
 anchor_dir = "/share/home/fengxiaocheng/ychuang/LatentFusion/experiments/anchor_embeddings/"
-anchor_prefix = "llama2-13b_mistral-7b_200000anchors_seed1"
+anchor_prefix = "llama2-13b_mistral-7b_1000000anchors_seed1"
 output_path = f"/share/home/fengxiaocheng/ychuang/LatentFusion/experiments/anchor_embeddings/{anchor_prefix}.pt"
 
 files = [f for f in os.listdir(anchor_dir) if f.startswith(anchor_prefix) and f != anchor_prefix + '.pt']
