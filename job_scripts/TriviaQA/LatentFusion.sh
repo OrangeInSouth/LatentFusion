5f4ae2ecf216c7436895a6f42e39b406a6ee0fd1
ychuang <ychuang@ir.hit.edu.cn>
2024-10-11 23:47:23 +0800

    debug

--- job_scripts/TriviaQA/LatentFusion.sh
+++ job_scripts/TriviaQA/LatentFusion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J LatentFusion-TriviaQA-Dev                               # 作业名为 test
 #SBATCH -o /share/home/fengxiaocheng/ychuang/LatentFusion/job_scripts/TriviaQA/test.out                          # stdout 重定向到 test.out
-#SBATCH -e c                          # stderr 重定向到 test.err
+#SBATCH -e /share/home/fengxiaocheng/ychuang/LatentFusion/job_scripts/TriviaQA/test.err                          # stderr 重定向到 test.err
 #SBATCH -p gpu02                            # 作业提交的分区为 compute
 #SBATCH -N 1                                  # 作业申请 1 个节点
 #SBATCH -t 8:00:00                            # 任务运行的最长时间为 1 小时
@@ -42,7 +42,7 @@ for weight1 in 0.0001 0.8 0.7 0.5; do
     weight2=$(echo "scale=4; 1.0 - $weight1" | bc)
 
     python src/main.py --config confs/TriviaQA/llama2-13b_mistral-7b.json \
-    --models llama2-13b mistral-7b \
+    --models ${tgt_model} ${src_model} \
     --layer-alignment $tgt_layer $src_layer \
     --anchors-path ${proj_path}/experiments/anchor_embeddings/llama2-13b_mistral-7b_1000000anchors_seed1_bug.pt \
     --embedding-projection-path ${proj_path}/experiments//embedding_projection/${tgt_model}_${src_mdoel}/EstimationEmbeddingProjection_${anchor_num}anchors_seed1_layer${tgt_layer}-${src_layer}.pt \
