5518c81fc7b1a98626c629ec59ea03e8cd5fdc06
ychuang <ychuang@ir.hit.edu.cn>
2024-10-11 23:35:54 +0800

    debug

--- job_scripts/TriviaQA/LatentFusion.sh
+++ job_scripts/TriviaQA/LatentFusion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J LatentFusion-TriviaQA-Dev                               # 作业名为 test
 #SBATCH -o /share/home/fengxiaocheng/ychuang/LatentFusion/job_scripts/TriviaQA/test.out                          # stdout 重定向到 test.out
-#SBATCH -e /share/home/fengxiaocheng/ychuang/LatentFusion/job_scripts/TriviaQA/test.err                           # stderr 重定向到 test.err
+#SBATCH -e c                          # stderr 重定向到 test.err
 #SBATCH -p gpu02                            # 作业提交的分区为 compute
 #SBATCH -N 1                                  # 作业申请 1 个节点
 #SBATCH -t 8:00:00                            # 任务运行的最长时间为 1 小时
@@ -12,7 +12,7 @@
 source ~/.bashrc
 
 conda activate ychuang
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=0
 mode=dev
 seed=1
 anchor_num=1000000
